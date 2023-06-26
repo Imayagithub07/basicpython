df4d757f62bf818fc2710d94ecc45a1133870105
IMAYA BHARATHI <115388923+Imayagithub07@users.noreply.github.com>
2023-06-26 16:22:31 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,28 @@
+# print statement used for print the information in the console
+#print(' Imaya bharathi')
+# input statement is used for to ask the user to give the data or information ex : name
+#v=input(' ENTER YOUR NAME : ')
+print( 'WELCOME')
+#mutiple varibales and multiple inputs using split
+a,b,c=input(' ENTER YOUR NAME :'),input(' ENTER YOUR AGE :'),input(' ENTER YOUR GENDER :')
+print(' YOUR NAME    :',a)
+print(' YOUR AGE     :',b)
+print(' YOUR GENDER  :',c)
+print(' CHECK THE DETAILS ')
+#arithmetic operation in python
+d=int(input('ENTER A TWO NUMBERS TO CALCULATE ARTHIMATIC OPERATIONS  :'))
+e=int(input('ENTER A TWO NUMBERS TO CALCULATE ARTHIMATIC OPERATIONS  :'))
+# addition
+print('Addition               :',d+e)
+# subtraction
+print('Subtraction            :',d-e)
+#mutiplication
+print('Multiplication         :',d*e)
+#modulus which gives you remainder by dividing it 
+print('Remainder              :',d%e)
+#division
+print('Divsion                :',d/e)
+#floor division
+print('Floor                  :',d//e)
+#exponentiation 
+print('Exponentiation         :',d**e)
